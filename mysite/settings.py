f75d471a2426cbb0862ed26a8d4873ac0d350b82
shigaki daichi <shigaki@shigakinoMacBook.local>
2018-06-30 12:20:18 +0900

    dad

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,8 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
@@ -126,8 +131,6 @@ DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
-
-STATIC_ROOT = 'staticfiles'
 
 DEBUG = False
 
