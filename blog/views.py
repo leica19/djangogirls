769c537e2fa92ae21edd71ff5f9510e1b47677f2
shigaki daichi <shigaki@shigakinoMacBook.local>
2018-06-30 12:33:13 +0900

    mo

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.utils import  timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
